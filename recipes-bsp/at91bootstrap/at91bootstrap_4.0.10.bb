vishwamartur/meta-atmel

commit b1dc385d4a9010db85b5b2477d352ca348831ab8

optee-bsp: at91bootstrap: add optee support for sama7g5ek
Add a new compatible machine "sama7g5ek-optee-sd".

Signed-off-by: Dharma Balasubiramani <dharma.b@microchip.com>

recipes-bsp/at91bootstrap/at91bootstrap_4.0.10.bb
@@ -3,7 +3,18 @@ require at91bootstrap.inc
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://LICENSES/MIT.txt;md5=12c44f58fe16bd407f016e45950c2f3d"
 
-COMPATIBLE_MACHINE = '(sama5d3-xplained|sama5d3-xplained-sd|sama5d4-xplained|sama5d4-xplained-sd|sama5d2-xplained|sama5d2-xplained-sd|sama5d2-xplained-emmc|sama5d2-ptc-ek|sama5d2-ptc-ek-sd|sama5d27-som1-ek|sama5d27-som1-ek-sd|sama5d27-som1-ek-optee-sd|sama5d2-icp-sd|sam9x60ek|sam9x60ek-sd|sama5d27-wlsom1-ek-sd|sama5d29-curiosity-sd|sama7g5ek-sd|sama7g5ek-emmc|sama7g5ek-ospi|sam9x60-curiosity|sam9x60-curiosity-sd|sam9x75-curiosity|sam9x75-curiosity-sd|sam9x75eb|sam9x75eb-sd)'
+COMPATIBLE_MACHINE = '(sama5d2-icp-sd\
+|sama5d2-ptc-ek|sama5d2-ptc-ek-sd\
+|sama5d2-xplained|sama5d2-xplained-sd|sama5d2-xplained-emmc\
+|sama5d27-som1-ek|sama5d27-som1-ek-sd|sama5d27-som1-ek-optee-sd\
+|sama5d27-wlsom1-ek-sd\
+|sama5d3-xplained|sama5d3-xplained-sd\
+|sama5d4-xplained|sama5d4-xplained-sd\
+|sama7g5ek-sd|sama7g5ek-emmc|sama7g5ek-ospi|sama7g5ek-optee-sd\
+|sam9x60ek|sam9x60ek-sd\
+|sam9x60-curiosity|sam9x60-curiosity-sd\
+|sam9x75-curiosity|sam9x75-curiosity-sd|sam9x75eb|sam9x75eb-sd\
+)'
 
 SRC_URI = "git://github.com/linux4sam/at91bootstrap.git;protocol=https;branch=at91bootstrap-4.x"
 

recipes-bsp/at91bootstrap/at91bootstrap.inc
@@ -38,6 +38,7 @@ AT91BOOTSTRAP_CONFIG:sama5d2-icp-sd ??= "${AT91BOOTSTRAP_MACHINE}-bsrsd_uboot"
 AT91BOOTSTRAP_CONFIG:sam9x60ek ??= "${AT91BOOTSTRAP_MACHINE}nf_uboot"
 AT91BOOTSTRAP_CONFIG:sam9x60ek-sd ??= "${AT91BOOTSTRAP_MACHINE}sd_uboot"
 AT91BOOTSTRAP_CONFIG:sama7g5ek-sd ??= "${AT91BOOTSTRAP_MACHINE}-bsrsd_uboot"
+AT91BOOTSTRAP_CONFIG:sama7g5ek-optee-sd ??= "${AT91BOOTSTRAP_MACHINE}-opteesd_uboot"
 AT91BOOTSTRAP_CONFIG:sama7g5ek-emmc ??= "${AT91BOOTSTRAP_MACHINE}-bsremmc_uboot"
 AT91BOOTSTRAP_CONFIG:sama7g5ek-ospi ??= "${AT91BOOTSTRAP_MACHINE}df_qspi_uboot"
 AT91BOOTSTRAP_CONFIG:sam9x60-curiosity-sd ??= "${AT91BOOTSTRAP_MACHINE}sd_uboot"
@@ -60,6 +61,7 @@ AT91BOOTSTRAP_LOAD:sama5d4-xplained-sd ??= "sdboot-uboot"
 AT91BOOTSTRAP_LOAD:sama5d2-icp-sd ??= "sdboot-uboot"
 AT91BOOTSTRAP_LOAD:sam9x60ek-sd ??= "sdboot-uboot"
 AT91BOOTSTRAP_LOAD:sama7g5ek-sd ??= "sdboot-uboot"
+AT91BOOTSTRAP_LOAD:sama7g5ek-optee-sd ??= "sdboot-uboot"
 AT91BOOTSTRAP_LOAD:sama7g5ek-emmc ??= "sdboot-uboot"
 AT91BOOTSTRAP_LOAD:sama7g5ek-ospi ??= "dataflashboot-uboot"
 AT91BOOTSTRAP_LOAD:sam9x60-curiosity-sd ??= "sdboot-uboot"

commit 3f58ef7aed4e5e22276edb2268d4d9b930c669d4

recipes-bsp: at91bootstrap: Add back sama5d29-curiosity-sd
Add back sama5d29-curiosity-sd machine to the at91bootstrap recipe file to
fix build issues which was missed in original commit.

Fixes: b1dc385d4a9 ("optee-bsp: at91bootstrap: add optee support for sama7g5ek")
Signed-off-by: Charan Pedumuru <charan.pedumuru@microchip.com>
Signed-off-by: Hari Prasath Gujulan Elango <Hari.PrasathGE@microchip.com>

recipes-bsp/at91bootstrap/at91bootstrap_4.0.10.bb
@@ -8,6 +8,7 @@ COMPATIBLE_MACHINE = '(sama5d2-icp-sd\
 |sama5d2-xplained|sama5d2-xplained-sd|sama5d2-xplained-emmc\
 |sama5d27-som1-ek|sama5d27-som1-ek-sd|sama5d27-som1-ek-optee-sd\
 |sama5d27-wlsom1-ek-sd\
+|sama5d29-curiosity-sd\
 |sama5d3-xplained|sama5d3-xplained-sd\
 |sama5d4-xplained|sama5d4-xplained-sd\
 |sama7g5ek-sd|sama7g5ek-emmc|sama7g5ek-ospi|sama7g5ek-optee-sd\